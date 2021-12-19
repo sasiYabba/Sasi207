e6f6ba4d46f3a5e60612a9b44dae2bc99fff7d13
sasiYabba <93094359+sasiYabba@users.noreply.github.com>
2021-12-19 09:26:20 +0530

    Update Msasi.py

--- Msasi.py
+++ Msasi.py
@@ -21,15 +21,13 @@ except ImportError:
 ### USERAGENT ###
 ua = ('Mozilla/5.0 (Linux; Android 5.0; Lenovo A1000 Build/S100; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.121 Mobile Safari/537.36 [FB_IAB/MESSENGER;FBAV/110.0.0.14.69;]')
 ### Logo ###
-logo = """        *au : rozhak
-                 *fb : fb.com/rozhak.xyz
-               *gh : github.com/r0zhak          
-░██████╗░█████╗░░██████╗██╗
-██╔════╝██╔══██╗██╔════╝██║
-╚█████╗░███████║╚█████╗░██║
-░╚═══██╗██╔══██║░╚═══██╗██║
-██████╔╝██║░░██║██████╔╝██║
-╚═════╝░╚═╝░░╚═╝╚═════╝░╚═╝    *yt : youtube.com/rozhakid"""
+logo = """        *au : sasindu.hacke
+\033[31m░██████╗░█████╗░░██████╗██╗
+\033[32m██╔════╝██╔══██╗██╔════╝██║
+\033[93m╚█████╗░███████║╚█████╗░██║
+\033[37m░╚═══██╗██╔══██║░╚═══██╗██║
+\033[94m██████╔╝██║░░██║██████╔╝██║
+\033[94m╚═════╝░╚═╝░░╚═╝╚═════╝░╚═╝    *yt : https://youtube.com/channel/UCuZT4TcMeScSm56hPCK-46w """
 mbasic_h={"Host":"mbasic.facebook.com","cache-control":"max-age=0","upgrade-insecure-requests":"1","user-agent":ua,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8","accept-encoding":"gzip, deflate","accept-language":"id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"}
 free_h={"Host":"free.facebook.com","cache-control":"max-age=0","upgrade-insecure-requests":"1","user-agent":ua,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8","accept-encoding":"gzip, deflate","accept-language":"id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"}
 mfb_h={'Host': 'm.facebook.com', 'cache-control': 'max-age=0', 'upgrade-insecure-requests': '1', 'user-agent':ua, 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8', 'accept-encoding': 'gzip, deflate', 'accept-language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7'}
