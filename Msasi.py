94216bfb828815347cf062f3edee685bd4aaed5a
sasiYabba <93094359+sasiYabba@users.noreply.github.com>
2021-12-19 09:55:14 +0530

    Update Msasi.py

--- Msasi.py
+++ Msasi.py
@@ -66,7 +66,7 @@ def login():
     os.system('clear')
     print logo
     print('──────────────────────────────────────────')
-    print(\033[37m[1] Login Token')
+    print('\033[37m[1] Login Token')
     print('\033[91m[2] Login Cookies')
     print('\033[42m[3] Token/Cookies')
     print('\033[93m[0] leave')
